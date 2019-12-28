e731de6bfc78d1ad450512f02dc4965037649bc2
OskyEdz <snakehultosky@gmail.com>
2019-12-29 00:56:32 +0100

    + New event listener

--- src/se/peacehult/spigothello/MyListener.java
+++ src/se/peacehult/spigothello/MyListener.java
@@ -2,16 +2,29 @@ package se.peacehult.spigothello;
 
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class MyListener implements Listener {
 
-    //When a player joins, type in chat.
+    //When a player joins, type in chat to all players.
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Bukkit.broadcastMessage("Welcome " + event.getPlayer().getName() + " to the server!");
     }
 
+    //Executes before the second method because it has a much lower priority.
+    @EventHandler (priority = EventPriority.LOWEST)
+        public void onPlayerChat1(AsyncPlayerChatEvent event) {
+            System.out.println("Who am I?");
+            event.setCancelled(true);
+        }
 
+    // Will not execute unless another listener with a lower priority has uncancelled the event.
+    @EventHandler (priority = EventPriority.HIGHEST, ignoreCancelled = true)
+        public void onPlayerChat2(AsyncPlayerChatEvent event) {
+        System.out.println("This shouldn't be executing");
+    }
 }
